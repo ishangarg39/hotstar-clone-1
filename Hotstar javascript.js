ef97c1830cc062c295fcf0a0a6f3b3750f435308
ishangarg39 <83765411+ishangarg39@users.noreply.github.com>
2022-09-22 14:34:15 +0530

    Update Hotstar javascript.js

--- Hotstar javascript.js
+++ Hotstar javascript.js
@@ -108,9 +108,9 @@ videoCards.forEach((item,i) =>{
 
 //card sliders
     
-let cardContainers={...document.querySelectorAll('.card-container')};
-let preBtns={...document.querySelectorAll('.pre-btn')};
-let nxtBtns={...document.querySelectorAll('.nxt-btn')};
+let cardContainers=[...document.querySelectorAll('.card-container')];
+let preBtns=[...document.querySelectorAll('.pre-btn')];
+let nxtBtns=[...document.querySelectorAll('.nxt-btn')];
 
 cardContainers.forEach((item,i)=>{
     let containerDimensions=item.getBoundingClientRect();
